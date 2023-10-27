b508845918117fae68585641c6524660a376f1ec
Viktor Tanyi <vtan@users.noreply.github.com>
2023-10-27 19:34:13 +0200

    Fill in jump locations separately

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,10 @@ const std = @import("std");
 
 pub fn main() !void {
     const code = try std.io.getStdIn().readToEndAlloc(std.heap.page_allocator, 1024 * 1024 * 1024);
+
     var ops = try parse(code);
+    try fillJumpLocations(&ops);
+
     var interpreter = Interpreter.init();
     try interpreter.run(try ops.toOwnedSlice());
 }
@@ -19,22 +22,14 @@ const Op = union(enum) {
 
 fn parse(code: []const u8) !std.ArrayList(Op) {
     var ops = std.ArrayList(Op).init(std.heap.page_allocator);
-    var jump_stack = std.ArrayList(u32).init(std.heap.page_allocator);
     for (code) |char| {
         const op_opt: ?Op = switch (char) {
             '+' => Op.inc,
             '-' => Op.dec,
             '<' => Op.left,
             '>' => Op.right,
-            '[' => blk: {
-                try jump_stack.append(@intCast(ops.items.len));
-                break :blk Op{ .jumpIfZero = 0xDEADBEEF };
-            },
-            ']' => blk: {
-                const pair_index = jump_stack.pop();
-                ops.items[pair_index] = Op{ .jumpIfZero = @intCast(ops.items.len) };
-                break :blk Op{ .jumpBackIfNonZero = pair_index };
-            },
+            '[' => Op{ .jumpIfZero = 0xDEADBEEF },
+            ']' => Op{ .jumpBackIfNonZero = 0xDEADBEEF },
             '.' => Op.print,
             else => null,
         };
@@ -43,6 +38,24 @@ fn parse(code: []const u8) !std.ArrayList(Op) {
         }
     }
     return ops;
+}
+
+fn fillJumpLocations(ops: *std.ArrayList(Op)) !void {
+    var jump_stack = std.ArrayList(u32).init(std.heap.page_allocator);
+    for (ops.items, 0..) |*op, i| {
+        switch (op.*) {
+            .jumpIfZero => {
+                try jump_stack.append(@intCast(i));
+            },
+            .jumpBackIfNonZero => {
+                const pair_index = jump_stack.pop();
+                ops.items[pair_index].jumpIfZero = @intCast(i);
+                op.*.jumpBackIfNonZero = pair_index;
+            },
+            else => {},
+        }
+    }
+    // TODO: check if jump_stack is empty
 }
 
 const Interpreter = struct {
