ca396180f14956b8306963671fc81f1a573ca244
Viktor Tanyi <vtan@users.noreply.github.com>
2023-12-03 15:45:30 +0100

    aarch64: Use unsigned offset loads/stores & simplify

--- src/codegen_aarch64.zig
+++ src/codegen_aarch64.zig
@@ -14,11 +14,11 @@ pub fn gen(ops: []const bf.Op, builder: *jit.Builder) !void {
                 const amount12: i12 = @intCast(amount);
                 try builder.emit32s(&[_]u32{
                     // ldrb w9, [x19]
-                    load_reg32_byte(Regs.scratch, Regs.tape_ptr),
+                    load_reg32_byte(Regs.scratch, Regs.tape_ptr, 0),
                     // add w9, w9, amount
                     add32_immediate(Regs.scratch, Regs.scratch, @bitCast(amount12)),
                     // strb w9, [x19]
-                    store_reg32_byte(Regs.scratch, Regs.tape_ptr),
+                    store_reg32_byte(Regs.scratch, Regs.tape_ptr, 0),
                 });
             },
 
@@ -36,7 +36,7 @@ pub fn gen(ops: []const bf.Op, builder: *jit.Builder) !void {
             .jump_if_zero => {
                 try builder.emit32s(&[_]u32{
                     // ldrb w9, [x19]
-                    load_reg32_byte(Regs.scratch, Regs.tape_ptr),
+                    load_reg32_byte(Regs.scratch, Regs.tape_ptr, 0),
                     // ands wzr, w9, w9
                     and32_flags(Regs.zero, Regs.scratch, Regs.scratch),
                 });
@@ -51,7 +51,7 @@ pub fn gen(ops: []const bf.Op, builder: *jit.Builder) !void {
 
                 try builder.emit32s(&[_]u32{
                     // ldrb w9, [x19]
-                    load_reg32_byte(Regs.scratch, Regs.tape_ptr),
+                    load_reg32_byte(Regs.scratch, Regs.tape_ptr, 0),
                     // ands wzr, w9, w9
                     and32_flags(Regs.zero, Regs.scratch, Regs.scratch),
                 });
@@ -76,9 +76,9 @@ pub fn gen(ops: []const bf.Op, builder: *jit.Builder) !void {
             .write => {
                 try builder.emit32s(&[_]u32{
                     // ldrb w0, [x19]
-                    load_reg32_byte(Regs.arg0, Regs.tape_ptr),
+                    load_reg32_byte(Regs.arg0, Regs.tape_ptr, 0),
                     // ldr x9, [x20]
-                    load_reg64(Regs.scratch, Regs.env_ptr),
+                    load_reg64(Regs.scratch, Regs.env_ptr, 0),
                     // blr x9
                     branch_link_reg(Regs.scratch),
                 });
@@ -86,14 +86,12 @@ pub fn gen(ops: []const bf.Op, builder: *jit.Builder) !void {
 
             .read => {
                 try builder.emit32s(&[_]u32{
-                    // add x9, x20, 8
-                    add64_immediate(Regs.scratch, Regs.env_ptr, 8),
-                    // ldr x9, [x9]
-                    load_reg64(Regs.scratch, Regs.scratch),
+                    // ldr x9, [x20, 8]
+                    load_reg64(Regs.scratch, Regs.env_ptr, 8),
                     // blr x9
                     branch_link_reg(Regs.scratch),
                     // strb w0, [x19]
-                    store_reg32_byte(Regs.arg0, Regs.tape_ptr),
+                    store_reg32_byte(Regs.arg0, Regs.tape_ptr, 0),
                 });
             },
         }
@@ -118,9 +116,9 @@ pub fn genPrologue(builder: *jit.Builder) !void {
 pub fn genEpilogue(builder: *jit.Builder) !void {
     try builder.emit32s(&[_]u32{
         // ldp x19, x20, [sp], 16
-        load_pair64(Regs.tape_ptr, Regs.env_ptr, Regs.stack_ptr, 16),
+        load_pair64_post_index(Regs.tape_ptr, Regs.env_ptr, Regs.stack_ptr, 16),
         // ldp x30, xzr, [sp], 16
-        load_pair64(Regs.link_reg, Regs.zero, Regs.stack_ptr, 16),
+        load_pair64_post_index(Regs.link_reg, Regs.zero, Regs.stack_ptr, 16),
         // ret
         ret(Regs.link_reg),
     });
@@ -164,21 +162,22 @@ fn or64(dest: Reg, source1: Reg, source2: Reg) u32 {
     return (0b10101010_000 << 21) | reg_imm6_reg_reg(source2, 0, source1, dest);
 }
 
-fn load_reg32_byte(dest: Reg, base: Reg) u32 {
-    return (0b00111000011_11111_111_010 << 10) | reg_reg(base, dest);
+fn load_reg32_byte(dest: Reg, base: Reg, offset: u12) u32 {
+    return (0b0011100101 << 22) | imm12_reg_reg(offset, base, dest);
 }
 
-fn load_reg64(dest: Reg, base: Reg) u32 {
-    return (0b11111000_011_11111_111_010 << 10) | reg_reg(base, dest);
+fn load_reg64(dest: Reg, base: Reg, offset: u15) u32 {
+    const offset12: u12 = @intCast(@divExact(offset, 8));
+    return (0b1111100101 << 22) | imm12_reg_reg(offset12, base, dest);
 }
 
-fn load_pair64(dest1: Reg, dest2: Reg, base: Reg, offset: i32) u32 {
+fn load_pair64_post_index(dest1: Reg, dest2: Reg, base: Reg, offset: i32) u32 {
     const offset7: i7 = @intCast(@divExact(offset, 8));
     return (0b1010100011 << 22) | imm7_reg_reg_reg(@bitCast(offset7), dest2, base, dest1);
 }
 
-fn store_reg32_byte(source: Reg, base: Reg) u32 {
-    return (0b00111000001_11111_111_010 << 10) | reg_reg(base, source);
+fn store_reg32_byte(source: Reg, base: Reg, offset: u12) u32 {
+    return (0b0011100100 << 22) | imm12_reg_reg(offset, base, source);
 }
 
 fn store_pair64_pre_index(dest1: Reg, dest2: Reg, base: Reg, offset: i32) u32 {
